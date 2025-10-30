5c0f365387b60f42f6f8a635fd98ff0ae866bd57
cristian gianoboli <cristian.gianoboli@artekium.com>
2025-10-30 15:54:11 -0300

    set the default theme to light

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased body`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
